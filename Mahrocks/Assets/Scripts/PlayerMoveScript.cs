1bbc563ca5f307f79ee522ecd2d852eaa934851a
Leonardo Souza <leonardostsouza@gmail.com>
2017-04-02 17:28:14 -0300

    Implements basic player and camera movement
    MIplementation of basic player movemente with w and s keys for forward
    and backward movement and a and d keys for rotation.
    
    Camera is a child of player object and follows player

---
+++ Mahrocks/Assets/Scripts/PlayerMoveScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour {
+
+    Rigidbody player;
+    public float playerMovementSpeed;
+    public float playerRotationSpeed;    
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float translate = (Input.GetAxis("Vertical") * playerMovementSpeed) * Time.deltaTime;
+        float rotate = (Input.GetAxis("Horizontal") * playerRotationSpeed) * Time.deltaTime;
+
+        transform.Translate(0, 0, translate);
+        transform.Rotate(0, rotate, 0);
+    }
+}
