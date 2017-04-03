59053044a085465debb39fa3d4aa2b3db4d0e945
phasath <sathler93@gmail.com>
2017-04-02 23:30:03 -0300

    Add Movement with Physics to player and Camera.

--- Mahrocks/Assets/Scripts/CameraYawnScript.cs
+++ Mahrocks/Assets/Scripts/CameraYawnScript.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraYawnScript : MonoBehaviour {
+public class CameraYawnScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+	public Transform target;
+	public float smoothing = 5f;
+	public float turnSpeed = 4.0f;
+
+
+	Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - target.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate ()
+	{
+		Vector3 targetCamPos = target.position + offset;
+		Vector3 targetCamRot = Camera.main.ScreenToViewportPoint (Input.mousePosition - target.position);
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+		transform.RotateAround (transform.position, transform.right, -targetCamRot.y * turnSpeed);
+		transform.RotateAround (transform.position, Vector3.up, targetCamRot.x * turnSpeed);
 	}
+
 }
--- Mahrocks/Assets/Scripts/PlayerMoveScript.cs
+++ Mahrocks/Assets/Scripts/PlayerMoveScript.cs
@@ -2,23 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveScript : MonoBehaviour {
+public class PlayerMoveScript : MonoBehaviour
+{
 
-    Rigidbody player;
-    public float playerMovementSpeed;
-    public float playerRotationSpeed;    
+	Rigidbody playerRB;
+	//Reference to the player's rigidbody
+	Vector3 movement;
+	//Vector for get the move position
 
-	// Use this for initialization
-	void Start () {
+	public float playerMovementSpeed;
+	//The speed that the player will move at
+	public float playerRotationSpeed;
+	//The speed that the player will rotate at
 
-    }
-	
+	int floorMask;
+
+	void Awake ()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		playerRB = GetComponent<Rigidbody> ();
+		Debug.Log ("Awaken");
+	}
+
 	// Update is called once per frame
-	void Update () {
-        float translate = (Input.GetAxis("Vertical") * playerMovementSpeed) * Time.deltaTime;
-        float rotate = (Input.GetAxis("Horizontal") * playerRotationSpeed) * Time.deltaTime;
+	void FixedUpdate ()
+	{
 
-        transform.Translate(0, 0, translate);
-        transform.Rotate(0, rotate, 0);
-    }
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+
+		//float translate = (Input.GetAxis("Vertical") * playerMovementSpeed) * Time.deltaTime;
+		//float rotate = (Input.GetAxis("Horizontal") * playerRotationSpeed) * Time.deltaTime;
+		//transform.Translate (0, 0, translate);
+		//transform.Rotate (0, rotate, 0);
+	}
+
+	void Move (float h, float v)
+	{
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * playerMovementSpeed * Time.deltaTime;
+
+		playerRB.MovePosition (transform.position + movement);
+	}
+
+	void Turning ()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, floorMask)) {
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRB.MoveRotation (newRotation);
+		}
+	}
 }
