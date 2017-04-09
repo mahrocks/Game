eea1237720096dd30449b979d7517dd797c6b5f5
Marcos <maasouza@poli.ufrj.br>
2017-04-09 13:07:43 -0300

    Spawn Areas and Enemy IA
    *Create simple IA to enemies follow the player
    *Create spawn areas with script to spawn any kind of enemies

---
+++ Mahrocks/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    Transform tr_Player;
+    float f_RotSpeed = 4.0f, f_MoveSpeed = 4.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        tr_Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Look at Player*/
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(tr_Player.position - transform.position), f_RotSpeed * Time.deltaTime);
+
+        /* Move at Player*/
+        transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
+    }
+
+}
+
---
+++ Mahrocks/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+    public GameObject[] enemy;
+    public Transform enemyPos;
+    public int numEnemies = 0;
+    public int maxEnemies = 5;
+    private float repeatRate = 1.5f;
+    public float minWait = 1.0f;
+    public float maxWait = 3.0f;
+    public float passedTime = 0.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            numEnemies = 0;
+            foreach(GameObject g in GameObject.FindGameObjectsWithTag("PlaneEnemies"))
+            {
+                numEnemies++;
+            }
+            if (numEnemies < maxEnemies)
+            {
+                if (passedTime < maxWait  && passedTime > minWait)
+                {
+
+                    if (Random.Range(0.0f, 1.0f) >= 0.5f)
+                    {
+                        Instantiate(enemy[Random.Range(0,2)], enemyPos.position, enemyPos.rotation);
+                        passedTime = 0.0f;
+                    }
+                }else{
+                    if (passedTime > maxWait)
+                    {
+                        Instantiate(enemy[Random.Range(0,2)], enemyPos.position, enemyPos.rotation);
+                        passedTime = 0.0f;
+
+                    }else
+                    {
+                        passedTime += Time.deltaTime;
+
+                    }
+                }
+            }
+        }
+    }
+	
+
+}
