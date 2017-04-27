c6b644a823954245e287d1660bd3e40b86f3e040
Marcos <maasouza@poli.ufrj.br>
2017-04-27 16:10:40 -0300

    Guitar update

--- Mahrocks/Assets/Scripts/BlulletSpawner.cs
+++ Mahrocks/Assets/Scripts/BlulletSpawner.cs
@@ -7,20 +7,30 @@ public class BlulletSpawner : MonoBehaviour {
 	public GameObject[] bullets;
 	public Transform spawnPos;
 	public AudioSource[] sounds;
+	public float secondsBetweenAttacks = 1.0f;
+
+	private float passedTime;
+
+	void Start(){
+		passedTime = 0.0f;
+	}
 
 	void Update() {
 
-		if (Input.GetButtonDown("Fire1")) {
-			if (!sounds[0].isPlaying && !sounds[1].isPlaying) {
+		passedTime += Time.deltaTime;
+	
+		if (Input.GetButtonDown("Fire1") && (passedTime > secondsBetweenAttacks)) {
+			//if (!sounds[0].isPlaying && !sounds[1].isPlaying) {
 
-				GameObject bullet = bullets [Random.Range (0, bullets.Length)];
-				bullet = (GameObject)Instantiate (bullet, spawnPos.position, spawnPos.transform.rotation);
-				sounds [Random.Range (0, sounds.Length)].Play ();
-				bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * 15;
+			passedTime = 0.0f;
+			GameObject bullet = bullets [Random.Range (0, bullets.Length)];
+			bullet = (GameObject)Instantiate (bullet, spawnPos.position, spawnPos.transform.rotation);
+			sounds [Random.Range (0, sounds.Length)].Play ();
+			bullet.GetComponent<Rigidbody> ().velocity = bullet.transform.forward * 30;
 
-				// Destroy the bullet after 2 seconds
-				Destroy (bullet, 2.0f); 
-			}
+			// Destroy the bullet after 2 seconds
+			Destroy (bullet, 2.0f); 
+			//}
 		}
 	}
 }
