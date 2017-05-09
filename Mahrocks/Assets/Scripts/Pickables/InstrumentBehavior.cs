40a5e78fbd6c02872cfbea0fd7ed4a8fd9ef598e
Leonardo Souza <leonardostsouza@gmail.com>
2017-05-09 19:52:49 -0300

    Implements instrument picking and basic victory mechanic

---
+++ Mahrocks/Assets/Scripts/Pickables/InstrumentBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstrumentBehavior : MonoBehaviour {
+	public int rotateSpeed = 30;
+	private GameObject playerObject;
+	private PlayerScore ps;
+
+	void Start(){
+		playerObject = GameObject.FindGameObjectWithTag ("Player");
+		ps = playerObject.GetComponent <PlayerScore> ();
+
+		ps.incrementInstrumentQuantity ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(0, rotateSpeed, 0) * Time.deltaTime);
+	}
+}
---
+++ Mahrocks/Assets/Scripts/Player/PlayerScore.cs
@@ -0,0 +1,73 @@
+﻿/* This script is responsible for controlling how many instruments the player has taken */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour {
+
+	private uint instrumentQuantity;
+	private uint collectedInstruments;
+	private bool playerWon;
+
+	public AudioSource collectInstrumentSound;
+	public AudioSource playerVictorySound;
+
+	public RawImage victoryImage;
+
+	// Use this for initialization
+	void Awake () {
+		// this values should be attributed BEFORE any call to incrementInstrumentsInGame in other scripts
+		instrumentQuantity = 0;
+		collectedInstruments = 0;
+	}
+
+	void Start(){
+		playerWon = false;
+		victoryImage.color = Color.clear;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((collectedInstruments == instrumentQuantity) && !playerWon) {
+			win ();
+		}
+	}
+
+	private void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Instrument"))
+		{
+			Destroy (other.gameObject);
+			incremetCollectedInstruments();
+			collectInstrumentSound.Play ();
+		}
+	}
+
+	// When an instrument is created, it calls this method to indicate it exists.
+	// This method must be called by other scripts ONLY on Start(). If it is called on Awake(),
+	// instrument count value may become corrupted
+	public void incrementInstrumentQuantity(){
+		instrumentQuantity++;
+	}
+
+	public uint getInstrumentQuantity(){
+		return instrumentQuantity;
+	}
+
+	// Called every time an instrument is picked up
+	public void incremetCollectedInstruments(){
+		collectedInstruments++;
+	}
+
+	public uint getCollectedInstruments(){
+		return collectedInstruments;
+	}
+
+	public void win(){
+		playerWon = true;
+		playerVictorySound.Play ();
+		victoryImage.color = new Color (1.0f, 1.0f, 1.0f, 1.0f);
+	}
+}
