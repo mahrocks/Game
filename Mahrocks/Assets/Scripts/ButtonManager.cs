ad5f5871b2a0cf7939adadfa47305c54e3073775
Marcos <maasouza@poli.ufrj.br>
2017-05-18 21:57:55 -0300

    Menu Scene and new models
    Menu Scene components
    Player model and enemies models (not playable yet)

---
+++ Mahrocks/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+	public void starGame(string newGameScene){
+		SceneManager.LoadScene (newGameScene);
+	}
+}
